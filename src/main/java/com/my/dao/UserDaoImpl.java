5070652139e4a774e6a46d411aa600902b67c357
Vlad Lastname <warangel.alk@gmail.com>
2014-07-19 12:03:29 +0300

    Implemented UserDaoImpl

--- src/main/java/com/my/dao/UserDaoImpl.java
+++ src/main/java/com/my/dao/UserDaoImpl.java
@@ -3,16 +3,23 @@ package com.my.dao;
 import com.my.dao.interfaces.UserDao;
 import com.my.model.User;
 import org.springframework.orm.hibernate4.HibernateTemplate;
+import org.springframework.util.DigestUtils;
+
+import java.util.Date;
 
 public class UserDaoImpl extends HibernateTemplate implements UserDao {
 
     @Override
     public long addUser(User user) {
-        return -1L;
+        user.setRegistrationDate(new Date());
+        // Replace password with it's md5 hash
+        user.setPass(DigestUtils.md5DigestAsHex(user.getPass().getBytes()));
+
+        return (Long) save(user);
     }
 
     @Override
     public User getUser(long id) {
-        return new User();
+        return get(User.class, id);
     }
 }
