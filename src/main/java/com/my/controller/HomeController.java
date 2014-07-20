119d2b9f59f09366f0a461bb5505cd0a58af2206
Vlad Lastname <warangel.alk@gmail.com>
2014-07-20 16:46:55 +0300

    Autowired Dao's into controllers

--- src/main/java/com/my/controller/HomeController.java
+++ src/main/java/com/my/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.my.controller;
 
+import com.my.dao.interfaces.PostDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private PostDao postDao;
 
     @RequestMapping(value = {"/", "/home"}, method = RequestMethod.GET)
     public String showHomePage(Model model) {
