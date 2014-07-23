11fa47505ec89195ee489eda9907f1818ac5b078
Vlad Lastname <warangel.alk@gmail.com>
2014-07-23 12:25:56 +0300

    Added Transactional annotations to Dao implementation classes methods.

--- src/main/java/com/my/dao/PostDaoImpl.java
+++ src/main/java/com/my/dao/PostDaoImpl.java
@@ -3,6 +3,7 @@ package com.my.dao;
 import com.my.dao.interfaces.PostDao;
 import com.my.model.Post;
 import org.springframework.orm.hibernate4.HibernateTemplate;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 import java.util.List;
@@ -12,6 +13,7 @@ public class PostDaoImpl extends HibernateTemplate implements PostDao {
     private static final int DEFAULT_POSTS_PER_PAGE = 10;
 
     @Override
+    @Transactional(readOnly = true)
     public List<Post> getPosts() {
         return loadAll(Post.class);
     }
@@ -22,6 +24,7 @@ public class PostDaoImpl extends HibernateTemplate implements PostDao {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Post> getPostsForPage(int page, int postsPerPage) {
         return this.<List<Post>>executeWithNativeSession(
                 session -> session
@@ -32,17 +35,20 @@ public class PostDaoImpl extends HibernateTemplate implements PostDao {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Post getPost(long id) {
         return get(Post.class, id);
     }
 
     @Override
+    @Transactional(readOnly = false)
     public long addPost(Post post) {
         post.setPostDate(new Date());
         return (Long) save(post);
     }
 
     @Override
+    @Transactional(readOnly = false)
     public void updatePost(Post post) {
         merge(post);
     }
