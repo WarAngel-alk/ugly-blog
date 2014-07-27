dfaf3f8c1f25fca8f64fc3125ceb038782506793
Vlad Lastname <warangel.alk@gmail.com>
2014-07-27 17:38:41 +0300

    Implemented method in PostController for adding comment to post.

--- src/main/java/com/my/controller/PostController.java
+++ src/main/java/com/my/controller/PostController.java
@@ -1,10 +1,14 @@
 package com.my.controller;
 
+import com.my.dao.interfaces.CommentDao;
 import com.my.dao.interfaces.PostDao;
+import com.my.dao.interfaces.UserDao;
 import com.my.model.Comment;
 import com.my.model.Post;
 import com.my.model.Tag;
+import com.my.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +20,12 @@ public class PostController {
 
     @Autowired
     private PostDao postDao;
+
+    @Autowired
+    private UserDao userDao;
+
+    @Autowired
+    private CommentDao commentDao;
 
     @RequestMapping(value = "/post/{id}", method = RequestMethod.GET)
     public String showPost(@PathVariable("id") long id, Model model) {
@@ -78,6 +88,21 @@ public class PostController {
         postDao.deletePost(post);
 
         return "redirect:/home";
+    }
+
+    @RequestMapping(value = "/post/{postId}/comment*", method = RequestMethod.PUT)
+    public String addComment(@PathVariable("postId") long postId,
+                             @ModelAttribute("newComment") Comment comment,
+                             Model model) {
+        User author = userDao.getUser(SecurityContextHolder.getContext().getAuthentication().getName());
+        comment.setAuthor(author);
+
+        Post post = postDao.getPost(postId);
+        comment.setPost(post);
+
+        commentDao.addComment(comment);
+
+        return "redirect:/post/" + postId;
     }
 
 }
