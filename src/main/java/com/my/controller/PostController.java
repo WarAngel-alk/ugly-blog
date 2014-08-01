b5c78ed5122e642f0f8ac2de6021ce6f823229d7
Vlad Lastname <warangel.alk@gmail.com>
2014-08-01 09:22:06 +0300

    Updated PostController: methods addPost and updatePost now process tagsString correctly; fixed errors while saving/updating post with changing tags; tagsString processing extracted to separate method.

--- src/main/java/com/my/controller/PostController.java
+++ src/main/java/com/my/controller/PostController.java
@@ -2,6 +2,7 @@ package com.my.controller;
 
 import com.my.dao.interfaces.CommentDao;
 import com.my.dao.interfaces.PostDao;
+import com.my.dao.interfaces.TagDao;
 import com.my.dao.interfaces.UserDao;
 import com.my.model.Comment;
 import com.my.model.Post;
@@ -14,6 +15,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
 @Controller
 public class PostController {
@@ -26,6 +30,9 @@ public class PostController {
 
     @Autowired
     private CommentDao commentDao;
+
+    @Autowired
+    private TagDao tagDao;
 
     @RequestMapping(value = "/post/{id}", method = RequestMethod.GET)
     public String showPost(@PathVariable("id") long id, Model model) {
@@ -51,35 +58,52 @@ public class PostController {
     public String addPost(@ModelAttribute("post") Post post,
                           @RequestParam("tagsString") String tagsString,
                           Model model) {
-        String[] tags = tagsString.split(", ");
-
-        for (String sTag : tags) {
-            Tag tag = new Tag(sTag);
-            post.getTags().add(tag);
-        }
+        processTags(post, tagsString);
 
         postDao.addPost(post);
 
         return "redirect:/post/" + post.getId();
     }
 
-    @RequestMapping(value = "/post/{id}", method = RequestMethod.POST)
+    @RequestMapping(value = "/post/{id}*", method = RequestMethod.POST)
     public String updatePost(
             @ModelAttribute("post") Post post,
-            @RequestParam("tagsString") String tagsString,
+            @RequestParam(value = "tagsString", defaultValue = "") String tagsString,
             @PathVariable("id") long id,
             Model model) {
-        String[] tags = tagsString.split(", ");
-
-        post.setTags(new ArrayList<Tag>(tags.length));
-        for (String sTag : tags) {
-            Tag tag = new Tag(sTag);
-            post.getTags().add(tag);
-        }
+        Date postDate = postDao.getPost(post.getId()).getPostDate();
+        post.setPostDate(postDate);
 
         postDao.updatePost(post);
 
+        processTags(post, tagsString);
         return "redirect:/post/" + post.getId();
+    }
+
+    private void processTags(Post post, String tagsString) {
+        if (!tagsString.isEmpty()) {
+            String[] tags = tagsString.split(",");
+
+            post.setTags(new ArrayList<Tag>(tags.length));
+
+            List<String> allNames = tagDao.getAllTagNames();
+            for (String sTag : tags) {
+                String tagName = sTag.trim();
+                Tag tag = null;
+                if (allNames.contains(tagName)) {
+                    tag = tagDao.getTag(tagName);
+                    if (!tag.getPosts().contains(post)) {
+                        tag.getPosts().add(post);
+                    }
+                } else {
+                    tag = new Tag(tagName);
+                    tag.setPosts(Arrays.asList(post));
+                }
+
+                post.getTags().add(tag);
+                tagDao.updateTag(tag);
+            }
+        }
     }
 
     @RequestMapping(value = "/post/{id}", method = RequestMethod.DELETE)
