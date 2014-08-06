8bfa1411d6cf064093ff3e0603b40fe878178967
Vlad Lastname <warangel.alk@gmail.com>
2014-08-06 18:28:15 +0300

    Added overridden BeforeClass method to AdminWebTest class to login as Admin

--- src/test/java/com/my/web/AdminWebTest.java
+++ src/test/java/com/my/web/AdminWebTest.java
@@ -1,6 +1,14 @@
 package com.my.web;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
 public class AdminWebTest extends AbstractWebTest {
 
+    @BeforeClass
+    @Override
+    public void loginBeforeClass() throws Exception {
+        loginInternal(admin_username, admin_password, true);
+    }
 
 }
