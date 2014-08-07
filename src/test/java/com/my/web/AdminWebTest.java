8fc1d5d7ce023705b6575af237a9959f0123bfc0
Vlad Lastname <warangel.alk@gmail.com>
2014-08-07 08:30:02 +0300

    Added constants to AdminWebTest class with data for created posts

--- src/test/java/com/my/web/AdminWebTest.java
+++ src/test/java/com/my/web/AdminWebTest.java
@@ -4,6 +4,10 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 public class AdminWebTest extends AbstractWebTest {
+    
+    private static final String testPostTitle = "TEST POST TITLE " + RandomStringUtils.randomNumeric(10);
+    private static final String testPostText = "TEST POST TEXT " + RandomStringUtils.randomNumeric(10);
+    private static final String testPostTags = "tag1, tag2";
 
     @BeforeClass
     @Override
