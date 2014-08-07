636771943c60c11db97e8d9d321dcca2e6283d21
Vlad Lastname <warangel.alk@gmail.com>
2014-08-07 08:31:33 +0300

    Added test to check post adding'

--- src/test/java/com/my/web/AdminWebTest.java
+++ src/test/java/com/my/web/AdminWebTest.java
@@ -1,7 +1,17 @@
 package com.my.web;
 
+import org.apache.commons.lang3.RandomStringUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class AdminWebTest extends AbstractWebTest {
     
@@ -21,4 +31,36 @@ public class AdminWebTest extends AbstractWebTest {
         logoutInternal();
     }
 
+    @Test
+    public void testAddPost() throws Exception {
+        driver.get(getAbsolutePath("/post/add"));
+
+        assertTrue(driver.getCurrentUrl().contains(getAbsolutePath("/post/add")));
+
+        WebElement titleField = driver.findElement(By.xpath(addPostForm_TitleField));
+        WebElement textField = driver.findElement(By.xpath(addPostForm_TextField));
+        WebElement tagsField = driver.findElement(By.xpath(addPostForm_TagsField));
+        WebElement submitButton = driver.findElement(By.xpath(addPostForm_SubmitButton));
+        
+        titleField.sendKeys(testPostTitle);
+        textField.sendKeys(testPostText);
+        tagsField.sendKeys(testPostTags);
+        submitButton.submit();
+
+        assertTrue(driver.getCurrentUrl().contains(getAbsolutePath("/post/")));
+
+        WebElement postTitle = driver.findElement(By.xpath(post_title));
+        WebElement postText = driver.findElement(By.xpath(post_text));
+        List<WebElement> tags = driver.findElements(By.xpath(post_tagsString));
+
+        assertTrue(postTitle.getText().contains(testPostTitle));
+        assertTrue(postText.getText().contains(testPostText));
+
+        List<String> testTagsList = Arrays.asList(testPostTags.split(", "));
+
+        assertEquals(tags.size(), testTagsList.size());
+        for (WebElement tag : tags) {
+            assertTrue(testTagsList.contains(tag.getText().trim()));
+        }
+    }
 }
