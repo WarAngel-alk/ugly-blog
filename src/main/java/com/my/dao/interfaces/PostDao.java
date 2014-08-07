f0dacf6647f8319c4831372302fdbfe2933febca
Vlad Lastname <warangel.alk@gmail.com>
2014-08-07 15:52:55 +0300

    Updated PostDao contact - added methods for selecting posts list by tag.

--- src/main/java/com/my/dao/interfaces/PostDao.java
+++ src/main/java/com/my/dao/interfaces/PostDao.java
@@ -1,6 +1,7 @@
 package com.my.dao.interfaces;
 
 import com.my.model.Post;
+import com.my.model.Tag;
 
 import java.util.List;
 
@@ -11,6 +12,12 @@ public interface PostDao {
     public List<Post> getPostsForPage(int page);
 
     public List<Post> getPostsForPage(int page, int postsPerPage);
+
+    public List<Post> getPostsByTag(Tag tag);
+
+    public List<Post> getPostsByTagForPage(Tag tag, int page);
+
+    public List<Post> getPostsByTagForPage(Tag tag, int page, int postsPerPage);
 
     public Post getPost(long id);
 
