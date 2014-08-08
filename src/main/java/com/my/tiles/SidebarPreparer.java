4dd51375d991fce67bbcd97cc1b17822ab423447
Vlad Lastname <warangel.alk@gmail.com>
2014-08-08 19:02:25 +0300

    Updated SidebarPreparer class - added autowired dao properties; execute() method fills set tagList and postList attributes.

--- src/main/java/com/my/tiles/SidebarPreparer.java
+++ src/main/java/com/my/tiles/SidebarPreparer.java
@@ -1,13 +1,30 @@
 package com.my.tiles;
 
+import com.my.dao.interfaces.PostDao;
+import com.my.dao.interfaces.TagDao;
+import com.my.model.Post;
+import com.my.model.Tag;
 import org.apache.tiles.AttributeContext;
 import org.apache.tiles.preparer.ViewPreparer;
 import org.apache.tiles.request.Request;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 public class SidebarPreparer implements ViewPreparer {
 
+    @Autowired
+    private TagDao tagDao;
+    @Autowired
+    private PostDao postDao;
+
     @Override
     public void execute(Request tilesContext, AttributeContext attributeContext) {
+        List<Tag> tagList = tagDao.getAllTags();
+        // TODO: Replace with 10 most rated posts
+        List<Post> postList = postDao.getPostsForPage(1, 10);
 
+        tilesContext.getContext("request").put("tagList", tagList);
+        tilesContext.getContext("request").put("postList", postList);
     }
 }
