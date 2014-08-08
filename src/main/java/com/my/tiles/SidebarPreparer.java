8bd2f92a73f44e292113fd787121dd110d463030
Vlad Lastname <warangel.alk@gmail.com>
2014-08-08 18:22:12 +0300

    Added com.my.tiles.SidebarPreparer class to add model attributed required in sidebar.jsp.

---
+++ src/main/java/com/my/tiles/SidebarPreparer.java
@@ -0,0 +1,13 @@
+package com.my.tiles;
+
+import org.apache.tiles.AttributeContext;
+import org.apache.tiles.preparer.ViewPreparer;
+import org.apache.tiles.request.Request;
+
+public class SidebarPreparer implements ViewPreparer {
+
+    @Override
+    public void execute(Request tilesContext, AttributeContext attributeContext) {
+
+    }
+}
