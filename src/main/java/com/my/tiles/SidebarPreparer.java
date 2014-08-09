2585f63635fd55dbe9ddb4673a475710820a8034
Vlad Lastname <warangel.alk@gmail.com>
2014-08-09 21:16:23 +0300

    SidebarPreparer passes to sidebar.jsp additional attribute with map of posts grouped by month (from PostUtil class).

--- src/main/java/com/my/tiles/SidebarPreparer.java
+++ src/main/java/com/my/tiles/SidebarPreparer.java
@@ -4,12 +4,14 @@ import com.my.dao.interfaces.PostDao;
 import com.my.dao.interfaces.TagDao;
 import com.my.model.Post;
 import com.my.model.Tag;
+import com.my.util.PostUtil;
 import org.apache.tiles.AttributeContext;
 import org.apache.tiles.preparer.ViewPreparer;
 import org.apache.tiles.request.Request;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.Map;
 
 public class SidebarPreparer implements ViewPreparer {
 
@@ -24,7 +26,12 @@ public class SidebarPreparer implements ViewPreparer {
         // TODO: Replace with 10 most rated posts
         List<Post> postList = postDao.getPostsForPage(1, 10);
 
+        Map<Integer, Map<Integer, List<Post>>> postMap = PostUtil.groupPostsByYearAndMonth(postDao.getPosts());
+
         tilesContext.getContext("request").put("tagList", tagList);
         tilesContext.getContext("request").put("postList", postList);
+        tilesContext.getContext("request").put("postMap", postMap);
     }
+
+
 }
